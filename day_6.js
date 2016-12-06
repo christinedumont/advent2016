a41d14a9ef4895e0055e7bd40f404f4035a78e65
cdumont <cdumont@8d.com>
2016-12-06 09:24:32 -0500

    completed p1

--- day_6.js
+++ day_6.js
@@ -6,22 +6,24 @@ var lines = dataFromFile('d6_data/puzzle.txt');
 processLines(lines);
 
 function processLines(lines) {
+  var result = '';
   var arr = new Array(lines[0].length);
   for (var i=0; i<lines.length; i++) {
     var line = lines[i];
-    for (var j=0; j<lines.length; j++) {
-      arr[j] += line.charAt(j);
+    if (line.length>0) {
+      for (var j=0; j<line.length; j++) {
+        if (arr[j]==undefined) arr[j] = '';
+        arr[j] += line.charAt(j);
+      }
     }
   }
-  var result = '';
-  for (var k=0; k<arr.length; k++) {
-    console.log(arr[k]+'\n\n');
-    var str = arr[k];
-    var sorted = str.match(/([a-z])\\1+/g);
-	  sorted.sort(function(a, b){
-		  return a.length> b.length? -1: 1;
-	  });
 
+  for (var k=0; k<arr.length; k++) {
+    var sorted = arr[k].split('');
+    sorted.sort((a,b) =>
+        sorted.filter(v => v===b).length
+        -sorted.filter(v => v===a).length
+    ).pop();
     result += sorted[0];
   }
 
