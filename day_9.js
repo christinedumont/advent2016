e32e22b1ccf229cf9b06a73999e888f35783fe53
cdumont <cdumont@8d.com>
2016-12-09 09:01:20 -0500

    day 9 p1 code

---
+++ day_9.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var chalk = require('chalk');
+var utils = require('./utils/utils.js');
+
+var data = utils.dataFromFile('d9_data/puzzle.txt')[0];
+solve(data);
+
+function solve(data) {
+  const markerRegex = /\(+(.+?)\)/;
+  var decompressed = '';
+  while (data.length != 0)
+  {
+    var nextMarker = markerRegex.exec(data);
+    if (nextMarker) {
+        var beforeMarker = data.substr(0, nextMarker.index);
+        var marker = data.substr(nextMarker.index, nextMarker[0].length);
+        var afterMarker = data.substr(nextMarker.index+nextMarker[0].length, data.length);
+
+        decompressed += beforeMarker;
+
+        var markerTarget = parseInt(nextMarker[1].split('x')[0]);
+        var markerRepeat = parseInt(nextMarker[1].split('x')[1]);
+
+        var toProcess = afterMarker.substring(0, markerTarget);
+        decompressed += toProcess.repeat(markerRepeat);
+        data = afterMarker.substring(markerTarget, data.length);
+    } else {
+      decompressed += data;
+      data = '';
+    }
+  }
+
+  decompressed = decompressed.trim();
+  console.log('Result P1 :: Decompressed length: '+decompressed.length);
+}
