b289041d8f9d92afb284db5c1a06c983cb25fcd3
cdumont <cdumont@8d.com>
2016-12-12 17:29:21 -0500

    new attempt to day 11

--- day_11.js
+++ day_11.js
@@ -2,6 +2,143 @@
 
 var chalk = require('chalk');
 
+var currentState = {
+  'floor' : 0,
+  'distance' : 0,
+  'pairs' : ['0:1', '0:2']
+}
+console.log('State: '+chalk.green(JSON.stringify(currentState)));
+
+var moves = new Array();
+var alreadyVisitedMoves = new Array();
+
+moves = possibleMoves(currentState, alreadyVisitedMoves);
+console.log(moves);
+/*while (moves.length>0) {
+  var move = moves.shift();
+  var newState = cloneMove(move);
+
+  if (move) {
+    newState = cloneMove(move);
+  }
+
+  if (newState && isDone(newState)) {
+    console.log('Min dist found for given input: '+newState.distance);
+    break;
+  } else {
+    moves = moves.concat(possibleMoves(currentState, alreadyVisitedMoves));
+  }
+}*/
+
+function possibleMoves(state, alreadyVisitedMoves) {
+  var possibles = new Array();
+
+  if (state.floor > 0) {
+    // try go down
+    var newMove = cloneMove(state);
+    newMove.floor --;
+    newMove.distance ++;
+
+    var possiblePairsMove = pairsWithElemOnFloor(state.floor, newMove.pairs);
+
+
+    /*if (isMoveLegal(newMove, alreadyVisitedMoves)) {
+      possibles.push(newMove);
+    }*/
+  }
+
+  if (state.floor < 3) {
+    // try go up
+    var newMove = cloneMove(state);
+    newMove.floor ++;
+    newMove.distance ++;
+
+    var possiblePairsMove = pairsWithElemOnFloor(state.floor, newMove.pairs);
+    // Single moves
+    for (var i=0; i<possiblePairsMove.length; i++) {
+      var possibleMove = cloneMove(newMove);
+      var newPairs = cloneArray(possiblePairsMove);
+      newPairs[i] = possiblePairsMove[i].replace(state.floor+'', newMove.floor+'');
+      possibleMove.pairs = newPairs;
+      if (isMoveLegal(possibleMove, alreadyVisitedMoves)) {
+        possibles.push(possibleMove);
+      }
+    }
+
+    // Double moves
+    for (var i=0; i<possiblePairsMove.length; i++) {
+
+    }
+
+
+  }
+
+  return possibles;
+}
+
+function isMoveLegal(move, aleadyVisitedMoves) {
+
+  // Move is not legal if this move was aleady done
+  var moveComp = move.floor+'|'+move.pairs;
+  for (var i=0; i<alreadyVisitedMoves.length; i++) {
+    if (moveComp == alreadyVisitedMoves[i]) return false;
+  }
+
+
+
+  // Move is not legal is something is getting fried
+  var pairs = move.pairs;
+  for (var i=0; i<pairs.length; i++) {
+    var pair1 = pairs[i];
+    for (var j=0; j<pairs.length; j++) {
+      var pair2 = pairs[j];
+      if (i!=j && pair1.split(':')[0] == pair2.split(':')[1]) return false;
+    }
+  }
+  return true;
+}
+
+function pairsWithElemOnFloor(floor, pairs) {
+  var goodPairs = new Array();
+  for (var i=0; i<pairs.length; i++) {
+    if (pairs[i].split(':')[0] == ''+floor || pairs[i].split(':')[1] == ''+floor) {
+        goodPairs.push(pairs[i]);
+    }
+  }
+  return goodPairs;
+}
+
+function addAlreadyVisited(state, aleadyVisitedMoves) {
+  aleadyVisitedMoves.push(state.floor+'|'+state.pairs);
+  return alreadyVisitedMoves;
+}
+
+function isDone(state) {
+  for (var i=0; i<state.pairs.length; i++) {
+    if (state.pairs[i] != '3:3') return false;
+  }
+  return true;
+}
+
+function cloneMove(move) {
+  var clone = {
+    'floor' : move.floor,
+    'distance' : move.distance,
+    'pairs' : cloneArray(move.pairs)
+  }
+  return clone;
+}
+
+function cloneArray(array) {
+  return array.map(function(arr) {
+    return arr.slice();
+  });
+}
+
+
+
+/*
+
 const maxData = 4;
 var testData = [['E', 'HM', 'LM'],['HG'],['LG'],[]];
 var puzzleData = [['E', 'SG', 'SM', 'PG', 'PM'],['RG', 'RM', 'CG', 'CM', 'TG'],['TM'],[]];
@@ -134,8 +271,6 @@ function pairs(arr) {
   return res;
 }
 
-function copy(current) {
-  return current.map(function(arr) {
-    return arr.slice();
-  });
-}
+
+
+*/
