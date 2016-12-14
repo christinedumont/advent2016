92247f1a50b6e760bc2e53e1de7288880a7db6fa
cdumont <cdumont@8d.com>
2016-12-14 17:00:22 -0500

    day 14 p2

--- day_14/day_14.js
+++ day_14/day_14.js
@@ -3,49 +3,59 @@
 let md5 = require('md5');
 let chalk = require('chalk');
 
-const testSalt = 'abc';
+const testSalt = 'abc';  //22728
 const salt = 'ngcjuoqr';
 
-generateKeys(testSalt);
+generateKeys(salt, true);
 
-function generateKeys(salt) {
+function generateKeys(salt, isPart2) {
   let confirmedKeys = [];
   let tempKeys = [];
 
   let idx = 0;
-  while (confirmedKeys.length<65) {
+  while (confirmedKeys.length<64) {
 
-    let hash = makeKeyHash(salt, idx);
+    tempKeys = cleanTempKeys(tempKeys, idx);
+    let hash = makeKeyHash(salt, idx, isPart2);
 
-    const reg = /(.)\1\1/;
+    const reg = /(.)\1{2,}/;
     let match = reg.exec(hash);
     if (match && match[0].length>2) {
-      let createKey = true;
-      if (match.length >= 5) {
-        // Could be a key confirmation, let's check
-        let confirmationIdx = isConfirmation(tempKeys, hash, idx, match[0].substring(0,5));
-        if (confirmationIdx != -1) {
-          createKey = false;
+
+      if (match[0].length >= 5) {
+        let newTempKeys = [];
+        for(let i=0; i<tempKeys.length; i++) {
+          if (tempKeys[i].triple == match[0].substring(0, 3)) {
+            console.log(chalk.magenta('Confirmed key ['+(confirmedKeys.length+1)+']: '+JSON.stringify((tempKeys[i]))+' at index: '+idx));
+            confirmedKeys.push(tempKeys[i]);
+          } else {
+            newTempKeys.push(tempKeys[i]);
+          }
         }
+        tempKeys = newTempKeys;
       }
 
-      if (createKey) {
-        // It's a new key
-        let keyObj = createKey(hash, idx, match[0].substring(0, 3));
-        console.log(chalk.green('Found key: '+JSON.stringify(keyObj)));
-        tempKeys.push(keyObj);
-      }
+      let keyObj = makeKey(hash, idx, match[0].substring(0, 3));
+    //  console.log(chalk.green('Key created: '+JSON.stringify(keyObj)));
+      tempKeys.push(keyObj);
     }
-
     idx ++;
   }
+
+  console.log(chalk.yellow('Key 64 is: '+JSON.stringify(confirmedKeys[63])));
 }
 
-function isConfirmation(tempKeys, hash, idx, fiveOfAKind) {
-
+function cleanTempKeys(tempKeys, currentIdx) {
+  let arr = [];
+  for(let i=0; i<tempKeys.length; i++) {
+    if ((tempKeys[i].idx+1000) > currentIdx) {
+      arr.push(tempKeys[i]);
+    }
+  }
+  return arr;
 }
 
-function createKey(hash, idx, triple) {
+function makeKey(hash, idx, triple) {
   return {
     'key' : hash,
     'idx' : idx,
@@ -53,6 +63,14 @@ function createKey(hash, idx, triple) {
   };
 }
 
-function makeKeyHash(salt, idx) {
-  return md5(salt+''+idx).toLowerCase();
+function makeKeyHash(salt, idx, isPart2) {
+  if (!isPart2) {
+    return md5(salt+''+idx).toLowerCase();
+  } else {
+    let hash = md5(salt+''+idx).toLowerCase();
+    for (let i=0; i<2016; i++) {
+      hash = md5(hash).toLowerCase();
+    }
+    return hash;
+  }
 }
