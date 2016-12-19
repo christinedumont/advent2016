dad1f09e4f5a0384f90f443fd8d2ec65620608f0
cdumont <cdumont@8d.com>
2016-12-19 13:06:14 -0500

    day 19 part one

---
+++ day_19/day_19.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let chalk = require('chalk');
+
+const testInput = 5;
+const input = 3001330;
+
+console.time("Time to distribute");
+distributePresents(testInput);
+console.timeEnd("Time to distribute");
+
+function distributePresents(input) {
+
+  console.log(chalk.red('Will shuffle presents for '+input+' elfs.'));
+
+  let elfs = buildElfList(input);
+  do {
+
+    console.time("Time for round");
+    for (let i=0; i < elfs.length; ++i) {
+      if (elfs[i].presents === 0) {
+        elfs.splice(i, 1); // Remove elf without present
+        --i; // Correct the index value
+      } else if (elfs.length>1) {
+        let elf1 = elfs[i];
+        let elf2 = (i+1 == elfs.length) ? elfs[0] : elfs[i+1];
+
+        elf1.presents = elf1.presents + elf2.presents;
+        elf2.presents = 0;
+      }
+    }
+
+    console.log(chalk.magenta(elfs.length)+chalk.yellow(' elfs left in circle.'));
+    console.timeEnd("Time for round");
+
+  } while (elfs.length != 1);
+
+  console.log(chalk.green('Winning elf is : '+JSON.stringify(elfs)));
+}
+
+function buildElfList(input) {
+  let elfs = [];
+  for (let i=0; i<input; i++) {
+    let elf = {
+      'id' : i+1,
+      'presents' : 1
+    };
+    elfs.push(elf);
+  }
+  return elfs;
+}
