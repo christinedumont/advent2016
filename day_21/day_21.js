7a57a6e1b631dea6e887a2f7f2520ed488a6fae6
cdumont <cdumont@8d.com>
2016-12-22 12:51:24 -0500

    day 21 code

--- day_21/day_21.js
+++ day_21/day_21.js
@@ -3,21 +3,26 @@
 let chalk = require('chalk');
 let utils = require('./../utils/utils.js');
 
-/*
-const input = 'abcdefgh';
-const instructions = utils.dataFromFile('d22_data/puzzle.txt');
-*/
 
+const input = 'abcdefgh';
+const instructions = utils.dataFromFile('d21_data/puzzle.txt');
+/*
 const input = 'abcde';
 const instructions = utils.dataFromFile('d21_data/test.txt');
-unscramble(input, instructions);
+*/
+//scramble(input, instructions, true);
 
-function unscramble(input, instructions) {
+unscramble('fbgdceah', instructions);
+
+function scramble(input, instructions, log) {
+  if (log) console.log(chalk.yellow(input));
 
   let result = input;
   for (let i=0; i<instructions.length; i++) {
     let instruction = instructions[i];
     if (instruction.trim().length>0) {
+
+      if (log) console.log(chalk.blue(instruction));
 
       // Possible instructions starts
       if (instruction.startsWith('swap position')) {
@@ -45,38 +50,128 @@ function unscramble(input, instructions) {
         result = setCharAt(result, pos2, char1)
 
       } else if (instruction.startsWith('reverse')) {
+        let matches = instruction.match(/\d+/g);
+
+        let pos1 = parseInt(matches[0]);
+        let pos2 = parseInt(matches[1]);
+
+        let stringP1 = result.substring(0, pos1);
+        let stringP2 = result.substring(pos1, pos2+1);
+        let stringP3 = result.substring(pos2+1, result.length);
+
+        let reversed = reverseString(stringP2);
+        result = stringP1 + reversed + stringP3;
 
       } else if (instruction.startsWith('rotate based')) {
+        let tokens = instruction.split(' ');
+        let letter = tokens[6];
+
+        let resultArr = result.split('');
+        let idx = resultArr.indexOf(letter);
+        let steps = 1+idx;
+
+        if (idx >= 4)
+          steps ++;
+
+        for (let j=0; j<steps; j++) {
+          result = shiftRight(result.split('')).join().replace(/,/g,'');
+        }
 
       } else if (instruction.startsWith('rotate')) {
+        let tokens = instruction.split(' ');
+        let step = parseInt(tokens[2]);
+
+        if (tokens[1] == 'left') {
+          for (let j=0; j<step; j++) {
+            result = shiftLeft(result.split('')).join().replace(/,/g,'');
+          }
+        } else { // right
+          for (let j=0; j<step; j++) {
+            result = shiftRight(result.split('')).join().replace(/,/g,'');
+          }
+        }
 
       } else if (instruction.startsWith('move position')) {
+        let matches = instruction.match(/\d+/g);
+        let pos1 = parseInt(matches[0]);
+        let pos2 = parseInt(matches[1]);
 
+        let resultArr = result.split('');
+        let toMove = result[pos1];
+
+        resultArr.splice(pos1, 1);
+        resultArr.splice(pos2, 0, toMove);
+
+        result = resultArr.join().replace(/,/g,'');
       }
-
+      if (log) console.log(chalk.yellow(result));
     }
   }
 
-  console.log(chalk.green('Final password is: ')+result);
+  if (log) console.log(chalk.green('Scrambled password is: ')+result);
+  return result;
+}
+
+function unscramble(input, instructions) {
+  let inputArr = input.split('');
+  let permutations = permutator(inputArr);
+
+  for (let i=0; i<permutations.length; i++) {
+    let start = permutations[i].join().replace(/,/g,'');
+    let scrambled = scramble(start, instructions, false);
+
+    if (scrambled == input) {
+      console.log(chalk.green('The initial scrambled password is: '+start));
+    }
+  }
 }
 
 function setCharAt(str,index,chr) {
-    if(index > str.length-1) return str;
-    return str.substr(0,index) + chr + str.substr(index+1);
+  if(index > str.length-1) return str;
+  return str.substr(0,index) + chr + str.substr(index+1);
 }
 
+function reverseString(str) {
+  let newString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+}
 
-/**
+function shiftRight(arr)
+{
+	var temp = new Array();
+	temp.push(arr[arr.length-1]);
+	for (let i = 0; i <= arr.length-2; i++) temp.push(arr[i]);
+	return temp;
+}
 
-swap position 4 with position 0 swaps the first and last letters, producing the input for the next step, ebcda.
-swap letter d with letter b swaps the positions of d and b: edcba.
-reverse positions 0 through 4 causes the entire string to be reversed, producing abcde.
-rotate left 1 step shifts all letters left one position, causing the first letter to wrap to the end of the string: bcdea.
-move position 1 to position 4 removes the letter at position 1 (c), then inserts it at position 4 (the end of the string): bdeac.
-move position 3 to position 0 removes the letter at position 3 (a), then inserts it at position 0 (the front of the string): abdec.
-rotate based on position of letter b finds the index of letter b (1), then rotates the string right once plus a number of times equal to that index (2): ecabd.
-rotate based on position of letter d finds the index of letter d (4), then rotates the string right once, plus a number of times equal to that index, plus an additional time because the index was at least 4, for a total of 6 right rotations: decab.
-After these steps, the resulting scrambled password is decab.
+function shiftLeft(arr)
+{
+	var temp = new Array();
+	for (let i = 1; i <= arr.length-1; i++) temp.push(arr[i]);
+	temp.push(arr[0]);
+	return temp;
+}
 
+function permutator(inputArr) {
+  var results = [];
 
-*/
+  function permute(arr, memo) {
+    var cur, memo = memo || [];
+
+    for (var i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0) {
+        results.push(memo.concat(cur));
+      }
+      permute(arr.slice(), memo.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+
+    return results;
+  }
+
+  return permute(inputArr);
+}
